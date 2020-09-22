e171982d4dd39611c7c035594cb0a66191766ed3
VivianaGuerraCustodio <vivianaguerracustodio@gmail.com>
2020-09-22 14:06:00 -0500

    moviendo la configuracion de firebase

--- firebaseConfig/index.js
+++ firebaseConfig/index.js
@@ -1,1 +1,20 @@
-/* eslint-disable no-unused-vars */
+
+import firebase from 'firebase'
+import 'firebase/firestore';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyChMZJ5MJGJseeu5IWtPMr_vuxKJxno-MM",
+  authDomain: "calle139.firebaseapp.com",
+  databaseURL: "https://calle139.firebaseio.com",
+  projectId: "calle139",
+  storageBucket: "calle139.appspot.com",
+  messagingSenderId: "1083905177180",
+  appId: "1:1083905177180:web:6683c9dd1bb26f958dd899",
+  measurementId: "G-WTSBTDF9TB"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+export const db = firebase.firestore();
+export default firebase;
--- src/main.js
+++ src/main.js
@@ -3,21 +3,8 @@ import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
-import firebase from 'firebase'
 import './registerServiceWorker'
-var firebaseConfig = {
-  apiKey: "AIzaSyChMZJ5MJGJseeu5IWtPMr_vuxKJxno-MM",
-  authDomain: "calle139.firebaseapp.com",
-  databaseURL: "https://calle139.firebaseio.com",
-  projectId: "calle139",
-  storageBucket: "calle139.appspot.com",
-  messagingSenderId: "1083905177180",
-  appId: "1:1083905177180:web:6683c9dd1bb26f958dd899",
-  measurementId: "G-WTSBTDF9TB"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+
 
 
 new Vue({
@@ -25,4 +12,4 @@ new Vue({
   render: (h) => h(App),
 }).$mount('#app');
 
-export default firebaseConfig;
+
