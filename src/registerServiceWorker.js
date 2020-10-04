10b9b880314bdb994bb03a87190f2384198d21a4
VivianaGuerraCustodio <vivianaguerracustodio@gmail.com>
2020-10-04 14:17:09 -0500

    configuration serviceworker

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -30,3 +30,14 @@ if (process.env.NODE_ENV === 'production') {
     }
   })
 }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
